96444453174ed18ab2945fb142ca9e94d8992f5d
Julien Descannevelle <jdesca.eirl@gmail.com>
2020-07-04 16:46:10 +0200

    commit

--- src/main/java/com/jdesca/wsdlgenerator/wsdlGenerator.java
+++ src/main/java/com/jdesca/wsdlgenerator/wsdlGenerator.java
@@ -52,6 +52,10 @@ public class wsdlGenerator {
             root.setAttribute("xmlns:xs", "http://www.w3.org/2001/XMLSchema");
             root.setAttribute("xmlns:tns", targetNamespace);
             root.setAttribute("targetNamespace", targetNamespace);
+            
+            //xs cschema element
+            Element schema = doc.createElement("xs:schema");
+            root.appendChild(schema);
 
             //add operationElements (messages, portType, binding)
             List<Element> messagesElements = new ArrayList<Element>();
@@ -86,6 +90,27 @@ public class wsdlGenerator {
                 portTypeOperation.appendChild(output);
                 
                 operationsElements.add(portTypeOperation);
+                
+                //binding operation
+                Element bindingOperation = doc.createElement("wsdl:operation");
+                bindingOperation.setAttribute("name", operation);
+                Element soapOpElement = doc.createElement("soap:operation");
+                soapOpElement.setAttribute("soapAction", targetNamespace+"/"+operation);
+                bindingOperation.appendChild(soapOpElement);
+                
+                Element inputElement = doc.createElement("wsdl:input");
+                Element inputBody = doc.createElement("soap:body");
+                inputBody.setAttribute("use", "literal");
+                inputElement.appendChild(inputBody);
+                
+                Element outputElement = doc.createElement("wsdl:output");
+                Element outputBody = doc.createElement("soap:body");
+                outputBody.setAttribute("use", "literal");
+                outputElement.appendChild(outputBody);
+                bindingOperation.appendChild(inputElement);
+                bindingOperation.appendChild(outputElement);
+                
+                bindingOperationsElement.add(bindingOperation);
             }
 
             //add operation elements to root
@@ -100,7 +125,33 @@ public class wsdlGenerator {
                 portType.appendChild(operation);
             }
             root.appendChild(portType);
+            
+            //binding tag
+            Element binding = doc.createElement("wsdl:binding");
+            binding.setAttribute("name", serviceName+"SOAP");
+            binding.setAttribute("type", "tns:"+serviceName);
+            
+            Element soapBinding = doc.createElement("soap:binding");
+            soapBinding.setAttribute("style", "document");
+            soapBinding.setAttribute("transport", "http://schemas.xmlsoap.org/soap/http");
+            binding.appendChild(soapBinding);
+            
+            for(Element wsdlOperation : bindingOperationsElement){
+                binding.appendChild(wsdlOperation);
+            }
 
+            //wsdl:service tag
+            Element service = doc.createElement("wsdl:service");
+            service.setAttribute("name", serviceName);
+            Element port = doc.createElement("wsdl:port");
+            port.setAttribute("binding", "tns:"+serviceName+"SOAP");
+            port.setAttribute("name", serviceName+"SOAP");
+            Element address = doc.createElement("wsdl:address");
+            address.setAttribute("location", "http://localhost:8080/"+ serviceName);
+            port.appendChild(address);
+            service.appendChild(port);
+            root.appendChild(service);
+            //append root to doc
             doc.appendChild(root);
             // write the content into xml file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -108,7 +159,7 @@ public class wsdlGenerator {
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");    
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("test.xml"));
+            StreamResult result = new StreamResult(new File(serviceName.concat(".wsdl")));
             transformer.transform(source, result);
 
             // Output to console for testing
